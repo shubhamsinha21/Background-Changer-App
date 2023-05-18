a3acd1d0e081fb8334bcd780a54ba574afebd110
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2023-05-18 14:04:10 +0530

    styling ✅

--- App.tsx
+++ App.tsx
@@ -1,12 +1,9 @@
 import React, { useState } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
   TouchableOpacity,
-  useColorScheme,
   View,
 } from 'react-native';
 
@@ -17,18 +14,24 @@ function App(): JSX.Element {
   //by writing JSX.Element, we are saying that the function only contains JSX
   //by default also, it allow only jsx
   
-  const [backgroundColor, setBackgroundColor] = useState("#ffffff")
+  const [randombackground, setRandomBackground] = useState("#ffffff")
 
   const generatorColor =() =>{
+   const hexRange = '0123456789ABCDEF'
+   let color='#'
+   for (let i = 0; i < 6; i++) {
+    color += hexRange[Math.floor(Math.random() * 16)];
+   }
 
+   setRandomBackground(color)
   }
 
 
   return (
     <>
     <StatusBar backgroundColor={'#00000'}/>
-    <View style={[styles.container]}>
-      <TouchableOpacity>
+    <View style={[styles.container, {backgroundColor:randombackground}]}>
+      <TouchableOpacity onPress={generatorColor}>
         <View style={styles.actionBtn}>
           <Text style={styles.actionBtnTxt}> Press me </Text>
         </View>
@@ -40,14 +43,20 @@ function App(): JSX.Element {
 
 const styles = StyleSheet.create({
   container:{
-    flex:1
-
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center'
   },
   actionBtn:{
-
+    borderRadius:20,
+    backgroundColor:'6A1B4D',
+    paddingVertical:10,
+    paddingHorizontal:40
   },
   actionBtnTxt:{
-
+    fontSize:20,
+    color:'#FFFFFF',
+   textTransform:'uppercase'
   }
 });
 
